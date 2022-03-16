13699b811c9cab1c2a2678ebf91013e4b55f86dd
fatmaSood <fsood53@gmail.com>
2022-03-16 06:26:55 -0700

    Write a program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "putchar.c"
+/**
+
+ * main -  prints Holberton, followed by a new line
+
+ * Return: Always 0 (Success)
+
+ */
+int main(void)
+{
+_putchar('P');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h')
+_putchar('a');
+_putchar('r');
+
+return (0);
+}
