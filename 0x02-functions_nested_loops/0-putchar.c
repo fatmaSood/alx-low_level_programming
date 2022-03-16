a33737093adf20279fa68f07751949c518e33f93
fsood <fsood53@gmail.com>
2022-03-16 16:31:10 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
-#include "putchar.c"
+#include "main.h"
 /**
 
- * main -  prints Holberton, followed by a new line
+ * main -  prints _putchar, followed by a new line
 
  * Return: Always 0 (Success)
 
  */
 int main(void)
 {
+_putchar('_');
 _putchar('P');
 _putchar('u');
 _putchar('t');
 _putchar('c');
-_putchar('h')
+_putchar('h');
 _putchar('a');
 _putchar('r');
+_putchar(/n);
 
 return (0);
 }
